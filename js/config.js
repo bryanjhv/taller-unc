3f0195a042d25fa7918bbd13bdfd050015592b92
Bryan Horna <bryanjhv@gmail.com>
2016-05-21 14:21:04 -0500

    Remove Google login

--- js/config.js
+++ js/config.js
@@ -14,39 +14,9 @@
     );
   }
 
-  // Get the multiplex secret
-  function getSecret(email) {
-    var trc = [
-      '\u0062\x72y\x61n\u006a',
-      'h\x76\u0040\u0067\x6d',
-      '\x61il.c\u006f\u006d'
-    ];
-    trc = trc.reduce(function (p, c) { return p + c; }).split('');
-
-    if (email && trc.every(function (c, i) { return email[i] === c; })) {
-      var start = 42; // answer to life, universe and everything!
-      var chars = [
-        0, 1002, -110, 12, 114, -60, 7, 56,
-        -65, 5, -6, -13, 39, '-d', '-2a',
-        36, 'g', '-b', -30, 7, '1i',
-        '-1e', 23, -3, '-k', '1o',
-        -34, 'k', 27, '-2d',
-        'i', '1q'
-      ];
-
-      return chars.map(function (_, $) {
-        return String.fromCharCode(start += parseInt(_, $ + 2));
-      }).join('\v').replace(/\v/g, '');
-    }
-
-    return null;
-  }
-
+  // Initialize reveal.js
   var $body = $('body');
-
-  var multiplex_src = '{{ cdn.reveal }}/plugin/multiplex';
-
-  var opts = {
+  Reveal.initialize({
     controls: true,
     progress: true,
     slideNumber: true,
@@ -89,27 +59,9 @@
       src: '{{ cdn.socketio }}/socket.io.min.js',
       async: true
     }, {
-      src: multiplex_src + '/client.min.js',
+      src: '{{ cdn.reveal }}/plugin/multiplex/client.min.js',
       async: true
     }]
-  };
-
-  Login.onUser(function (user) {
-    var secret = getSecret(user ? user.email : user);
-
-    if (secret) {
-      var child = $('[src="' + multiplex_src + '/client.min.js"]', 0);
-      if (child.isElem()) {
-        child = child.el;
-        $body.el.removeChild(child);
-      }
-
-      Reveal.getConfig().multiplex.secret = secret;
-      child = $('!script').attr({ src: multiplex_src + '/master.min.js' });
-      $body.append(child);
-    }
-  }, true);
-
-  Reveal.initialize(opts);
+  });
 
 }($$));
--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(function () {
 
   // We have JavaScript enabled
-  var body = $$('body');
-  body.className = body.className.replace(/no-js ?/, '');
+  $$('body').removeClass('no-js');
 
   // Cancel navigating to #
   $$('a[href="#"]').each(function (link) {
@@ -10,55 +9,5 @@ $(function () {
       e.preventDefault();
     });
   });
-
-  var userDiv = $$('#user');
-
-  if (userDiv.isElem()) {
-    Login.onUser(function (user) {
-
-      while (userDiv.el.firstChild) {
-        userDiv.el.removeChild(userDiv.el.firstChild);
-      }
-
-      var btnCls = 'btn waves-effect waves-light';
-
-      if (!user) {
-        var loginBtn = $$('!a').prop({
-          className: btnCls,
-          id: 'loginBtn',
-          innerHTML: 'Iniciar sesión'
-        });
-        auth2.attachClickHandler(loginBtn.el);
-
-        userDiv.append(loginBtn);
-      } else {
-        var html = '\n' +
-          '<div class="card-panel teal lighten-1 z-depth-1">' +
-          '  <div class="row valign-wrapper">' +
-          '    <div class="col s3">' +
-          '      <img src="$imageUrl" class="circle responsive-img">' +
-          '    </div>' +
-          '    <div class="col s9">' +
-          '      <span class="white-text">Sesión iniciada como<br>' +
-          '      <b>$email</b></span><br>' +
-          '      <a href="#" class="' + btnCls + '" id="logoutBtn">' +
-          '        Cerrar sesión</a>' +
-          '    </div>' +
-          '  </div>' +
-          '</div>';
-        html = html.replace(/\$(\w+)/g, function (m, p1) {
-          return user[p1];
-        });
-
-        userDiv.html(html);
-
-        $$('#logoutBtn').on('click', function onClick(e) {
-          e.preventDefault();
-          this.off('click', onClick, false);
-          auth2.signOut();
-        }, false);
-      }
-    });
-  }
 
 });
